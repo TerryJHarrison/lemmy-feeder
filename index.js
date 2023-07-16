fbab933ac06872af078934a7a93f1461c961d48a
TJ Harrison <tjharrisonjr@gmail.com>
2023-07-16 11:35:22 -0400

    Use username/pass to generate JWT. Prevent reposting keyed off post titles

--- index.js
+++ index.js
@@ -22,29 +22,39 @@ const post = (path, payload) => new Promise((resolve, reject) => {
 });
 
 exports.handler = async function(event) {
-  console.log("Loading feed: " + event.RSS_FEED);
-  let feed = await parser.parseURL(event.RSS_FEED);
-  console.log("Populating from " + feed.title + " for last 8 hours");
+    console.log("Loading feed: " + event.RSS_FEED);
+    let feed = await parser.parseURL(event.RSS_FEED);
+    console.log("Populating from " + feed.title + " for last 8 hours");
 
-  let postsToCreate = [];
-  feed.items.forEach(item => {
-    //TODO: de-duplicate from existing posts
-    //create a post promise for each item
-    postsToCreate.push(post('/api/v3/post', {
-      name: item.title,
-      community_id: event.COMMUNITY_ID,
-      language_id: event.LANGUAGE_ID,
-      url: item.link,
-      body: item.contentSnippet,
-      auth: process.env.LEMMY_JWT
+    let auth = JSON.parse(await post('/api/v3/user/login', {
+        username_or_email: process.env.LEMMY_USER,
+        password: process.env.LEMMY_PASS
     }));
-  });
-  console.log("Creating " + postsToCreate.length + " posts");
-  const response = await Promise.all(postsToCreate);
-  console.log(response);
+
+    let currentPosts = await parser.parseURL('/feeds/c/' + event.COMMUNITY_NAME + '.xml?sort=New');
+    let postTitles = currentPosts.items.map(post => post.title);
+
+    let postsToCreate = [];
+    feed.items.forEach(item => {
+        //Prevent reposts based on title
+        if(!postTitles.contains(item.title){
+            //Create a post promise for each new entry
+            postsToCreate.push(post('/api/v3/post', {
+                name: item.title,
+                community_id: event.COMMUNITY_ID,
+                language_id: event.LANGUAGE_ID,
+                url: item.link,
+                body: item.contentSnippet,
+                auth: auth.jwt
+            }));
+        }
+    });
+    console.log("Creating " + postsToCreate.length + " posts");
+    const response = await Promise.all(postsToCreate);
+    console.log(response);
   
-  return {
-    statusCode: 200,
-    message: "OK"
-  }
+    return {
+        statusCode: 200,
+        message: "OK"
+    }
 };
